44cc9e6104bd1f911c4dcfdc2c80072b5c606b12
Abuzar Tamboli <abuzart1999@gmail.com>
2025-10-21 23:38:04 +0530

    fix: ensure database connection is established before deleting a user

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db";
+import { User } from "../models/user.model";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "dev-space" });
@@ -30,6 +31,7 @@ const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
+    await connectDB();
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
     //TODO: Add more features here

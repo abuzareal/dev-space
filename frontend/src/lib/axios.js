861af44603552c44ca5b993b5ccab6f7a9f43ff1
Abuzar Tamboli <abuzart1999@gmail.com>
2025-10-22 15:05:50 +0530

    Auth provider and tools setup

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,16 @@
+// https://dev-space-backend.vercel.app
+
+import axios from "axios";
+
+// this was the first version, we just changed it at the end of the tutorial
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://dev-space-backend.vercel.app/api";
+
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});

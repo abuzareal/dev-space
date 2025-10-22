b31639cd66fec4e6bed34521a180d5178f62dc63
Abuzar Tamboli <abuzart1999@gmail.com>
2025-10-22 17:34:31 +0530

    Added custom hook - useStreamChat

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,9 +2,11 @@ import { UserButton } from "@clerk/clerk-react";
 import React from "react";
 
 const HomePage = () => {
-  return <div>
-    <UserButton/>
-  </div>;
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
 };
 
 export default HomePage;
